b2ec70efdfba8610ddc1181ce27029e70c79306a
juerock <7029281+juerock@users.noreply.github.com>
2026-08-16 16:12:49 +0000

    Deploy Valaxy site - bb484b2d61f84f4f4eb4fc75e47c2932e6799fa5 juerock/valaxy-jueorck@bb484b2d61f84f4f4eb4fc75e47c2932e6799fa5

---
+++ assets/index32.DbbbxLzh.js
@@ -0,0 +1,1 @@
+import{$ as e,W as t}from"./theme.BaHiKBte.js";import{p as n}from"./index3.BZxPwLvj.js";import{t as r}from"./schema-org.SD01-lvP.BMCF9ehy.js";var i=t({defaults:{"@type":`Review`},inheritMeta:[`inLanguage`],resolve(t,i){return t.reviewRating=e(t.reviewRating,i,r),t.author=e(t.author,i,n),t}});export{i as reviewResolver};
